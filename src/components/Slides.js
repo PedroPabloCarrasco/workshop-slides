495e9b0c7fcd80e99bb61c8335dddac7608b6fe9
PedroPablo <pedropablocarrascob@gmail.com>
2024-08-09 14:09:23 -0400

    Se agregan imagenes en el archivo App.js, en el archivo slides.js se genera un intervalo para mostrar las imagenes

---
+++ src/components/Slides.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+
+const Slides = ({ images }) => {
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    useEffect(() => {
+        const tick = setInterval(() => {
+            if (activeIndex < images.length - 1) {
+                setActiveIndex(prevIndex => prevIndex + 1);
+            } else {
+                // Vuelve al inicio que viene siendo el indice 0
+                setActiveIndex(0);
+            }
+        }, 1000);
+
+        return () => clearInterval(tick);
+    }, [activeIndex, images.length]);
+
+    return (
+        <div>
+            <h1>{activeIndex}</h1>
+            {/* Renderiza la imagen activa */}
+            <div>
+                <img src={images[activeIndex].src} alt={images[activeIndex].title} />
+                <div>{images[activeIndex].title}</div>
+            </div>
+        </div>
+    );
+};
+
+Slides.defaultProps = {
+    images: [
+        {
+            src: 'https://www.pexels.com/es-es/foto/naturaleza-cielo-noche-espacio-2387877/',
+            title: 'Vía Láctea'
+        },
+        {
+            src: 'https://images.pexels.com/photos/2753432/pexels-photo-2753432.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+            title: 'Universo'
+        },
+        {
+            src: 'https://www.pexels.com/es-es/foto/naturaleza-cielo-noche-espacio-2387877/',
+            title: 'Espacio'
+        }
+    ]
+};
+
+export default Slides;
